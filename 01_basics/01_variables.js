ea20a2577b613e61bd08e73687fafc4b44f0d733
bhanu987 <92867773+bhanu987@users.noreply.github.com>
2023-12-14 16:40:11 +0000

    studied about ways to declare variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345;
+let accountEmail = "abc@google.com";
+
+var accountPassword = 111111;
+
+accountCity="Umbergaon";
+
+let accountState;
+
+//accountId=3; not allowed 
+
+//console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+Prefer not to use 'var' because of issue in block and functional scope
+*/
