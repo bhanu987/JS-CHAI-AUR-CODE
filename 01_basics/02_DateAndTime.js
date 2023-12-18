cfabc7573f40606ce2f10705d6c80de69843a28c
bhanu987 <92867773+bhanu987@users.noreply.github.com>
2023-12-18 12:35:33 +0000

    Learned about date and time in Js

---
+++ 01_basics/02_DateAndTime.js
@@ -0,0 +1,12 @@
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toTimeString());
+
+console.log(myDate.getDate());
+console.log(myDate.getFullYear());
